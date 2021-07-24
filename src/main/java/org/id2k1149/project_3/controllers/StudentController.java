7b2519d3217f2ccdd00f1fee32c38acf6f25f875
id2k1149 <id2k1149@gmail.com>
2021-07-24 11:34:17 -0400

    v01 add Service

--- src/main/java/org/id2k1149/project_3/controllers/StudentController.java
+++ src/main/java/org/id2k1149/project_3/controllers/StudentController.java
@@ -1,27 +1,28 @@
 package org.id2k1149.project_3.controllers;
 
 import org.id2k1149.project_3.models.Student;
+import org.id2k1149.project_3.servise.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/students")
 public class StudentController {
 
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
     @GetMapping
     public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        1L,
-                        "John",
-                        "john@test.test",
-                        LocalDate.of(1940, Month.OCTOBER, 9),
-                        40)
-        ) ;
+        return studentService.getStudents();
     }
+
 }
