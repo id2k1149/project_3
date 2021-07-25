eb74b99602f8d435dadec0e6c194ac25ee904438
id2k1149 <id2k1149@gmail.com>
2021-07-24 20:11:55 -0400

    v06

--- src/main/java/org/id2k1149/project_3/controllers/StudentController.java
+++ src/main/java/org/id2k1149/project_3/controllers/StudentController.java
@@ -3,9 +3,7 @@ package org.id2k1149.project_3.controllers;
 import org.id2k1149.project_3.models.Student;
 import org.id2k1149.project_3.servise.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,4 +23,21 @@ public class StudentController {
         return studentService.getStudents();
     }
 
+    @PostMapping
+    public void registerNewStudent(@RequestBody Student student) {
+        studentService.addNewStudent(student);
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long studentId) {
+        studentService.deleteStudent(studentId);
+    }
+
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(
+            @PathVariable("studentId") Long studentId,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String email) {
+        studentService.updateStudent(studentId, name, email);
+    }
 }
